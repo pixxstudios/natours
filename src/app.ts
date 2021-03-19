6f13b9110b663a5f6a012f3ced89875af9394f87
Gagandeep Singh <gagan.ddun@gmail.com>
2021-03-20 00:04:13 +1100

    adds post request

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,20 @@ import express, { Request, Response, Application } from 'express';
 
 const app: Application = express();
 
+// middleware
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: "hello from ts" });
+});
+
+app.get('/api/v1/tours', (req: Request, res: Response) => {
+    res.status(200).json({ message: "all tours info" });
+});
+
+app.post('/api/v1/tours', (req: Request, res: Response) => {
+    console.log('>>> ', req.body);
+    res.status(200).send("Updated");
 });
 
 const PORT: number = 3000;

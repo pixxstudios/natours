2c974b7792fbc7e8899a7babd84efee51d772533
Gagandeep Singh <gagan.ddun@gmail.com>
2021-03-24 21:42:40 +1100

    Connect to DB

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 dotenv.config({ path: `${__dirname}/../config.env` });
 
 import app from './app';
+
+const DB: string = process.env.DATABASE!.replace('<PASSWORD>', process.env.DATABASE_PASSWORD!);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(con => {
+    console.log(con.connections);
+    console.log('DB Connection successful');
+});
 
 app.listen(process.env.PORT, () => {
     console.log('App running on port ', process.env.PORT)

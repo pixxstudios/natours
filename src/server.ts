67d4552d2c761a9fccb8c3a30b32fb39b17ab5cb
Gagandeep Singh <gagan.ddun@gmail.com>
2021-03-24 23:11:59 +1100

    Save a new tour

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,32 @@ mongoose.connect(DB, {
     console.log('DB Connection successful');
 });
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Forest Hiker',
+    rating: 4.7,
+    price: 498
+});
+
+testTour.save().then(doc => console.log(doc));
+
 app.listen(process.env.PORT, () => {
     console.log('App running on port ', process.env.PORT)
 });
